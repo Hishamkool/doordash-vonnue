d5ee94a6b68d305a182414c80441bfbb9695c02f
Hisham <linkedin.hisham@gmail.com>
2025-08-26 14:54:18 +0530

    styled tab bar section , added javascript for tab section , to see more items in the list , change arrow direction.

---
+++ javascript.js
@@ -0,0 +1,32 @@
+
+/* tab selection */
+const alltabs = document.querySelectorAll(".tabbar-head a");
+
+alltabs.forEach(currenttab => {
+    currenttab.addEventListener('click', e => {
+        alltabs.forEach(t => t.classList.remove('selected'));
+        e.preventDefault();
+        currenttab.classList.add('selected');
+    });
+});
+
+/* see more button and list visibility  */
+const seeMoreContent = document.querySelector('.see-more-top-cities');
+const seeMoreBtn = document.getElementById('see-more-btn');
+const seeMoreDiv = document.querySelector('.see-more');
+const seeMoreText = seeMoreDiv.querySelector('span');
+const arrow = document.getElementById('arrow');
+seeMoreBtn.addEventListener("click", k => {
+    seeMoreContent.classList.toggle('enable');
+    /* moving see more to the end of the list */
+    seeMoreDiv.parentNode.appendChild(seeMoreDiv);
+    arrow.classList.toggle('open');
+    if (seeMoreContent.classList.contains('enable')) {
+        seeMoreText.textContent="See less";
+        
+    }else{
+        
+        seeMoreText.textContent="See More";
+    }
+    console.log("clicked see more button");
+});
